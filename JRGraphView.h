c770288f979177f3194731192b969724975833e5
llguo <llguo@xtremeprog.com>
2015-02-01 20:54:55 +0800

    first commit

---
+++ JRGraphView.h
@@ -0,0 +1,13 @@
+//
+//  JRGraphView.h
+//  JRPlot
+//
+//  Created by __无邪_ on 15/1/29.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JRGraphView : UIView
+
+@end
---
+++ JRGraphView.m
@@ -0,0 +1,21 @@
+//
+//  JRGraphView.m
+//  JRPlot
+//
+//  Created by __无邪_ on 15/1/29.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import "JRGraphView.h"
+
+@implementation JRGraphView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
